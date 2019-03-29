49530b07df124b1a2fe5aa9b3023441935e5f960
kondors1995 <normandija1945@gmail.com>
2019-03-29 13:29:16 +0000

    mido: TARGET_PROVIDES_POWERHAL := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,6 +202,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
+TARGET_PROVIDES_POWERHAL := true
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
